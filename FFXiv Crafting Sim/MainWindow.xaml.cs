8983a31efc56496e0d10c459f9f52b26eeee3dd8
Darius <shadriu@gmail.com>
2020-03-16 15:01:24 +0200

    Add project files.

---
+++ FFXiv Crafting Sim/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FFXiv_Crafting_Sim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextChangedAllowNumericOnly(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (sender == null) return;
+            string text = textBox.Text;
+            textBox.Text = string.Concat(text.Where(x => char.IsDigit(x)));
+        }
+    }
+}
